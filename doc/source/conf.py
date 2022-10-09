a2ed80e85c3f79a50b78d22812ed72dccb27aa15
mjsandells <melody.sandells@nnorthumbria.ac.uk>
2022-10-09 20:06:56 +0100

    Adds napoleon to doc config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,8 +70,25 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon'
     ]
+
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
 autosummary_generate = True
 templates_path = ['_templates']
