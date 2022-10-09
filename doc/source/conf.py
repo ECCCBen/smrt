46351f035b4fb7e5a643a14e21b36bf2869d4b42
mjsandells <melody.sandells@nnorthumbria.ac.uk>
2022-10-09 17:39:43 +0100

    Change theme to sphinx_rtd_theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -191,7 +191,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'bizstyle'
-html_theme = 'karma_sphinx_theme'
+# html_theme = 'karma_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
